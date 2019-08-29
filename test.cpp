7b2c0de9e5ed35302d030f39f9d594f2cdff11f2
unknown <prabhat.avb@gmail.com>
2019-08-29 11:37:34 +0530

    first ver

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  char name[10]; int age;
+  int a = 10;
+  cout<< "Enter your name and age"<<endl;
+  cin>> name >> age;
+  cout<< "Your name is "<<name<<" and you are "<<age<<" years old";
+
+  return 0;
+}
